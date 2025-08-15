1a614e631a8c3bf1e72d35d08d3220c4934c9af8
William-Bos <boswilliam56@gmail.com>
2025-08-15 03:44:49 -0300

    atividades novas

---
+++ tasks.js
@@ -0,0 +1,57 @@
+/*
+
+let nome = prompt("Qual seu nome?\n");
+console.log("Boas vindas "  + nome );
+
+*/
+
+
+/*
+
+let numeroum = parseFloat(prompt("Mande seu primeiro número para soma \n"))
+let numerodois = parseFloat(prompt("Mande seu segundo número para soma\n"))
+let soma = numeroum + numerodois;
+console.log("O resultado de sua conta é\n" + soma);
+
+*/
+
+
+/*
+
+let idade = parseInt(prompt("Qual sua idade?\n"))
+let anos = 10;
+let futuro = idade + anos;
+console.log("Você tem " + futuro + " anos daqui a " + anos +" anos") 
+
+*/
+
+
+/*
+let f = 10;
+let c = (f - 32) * 5/9;
+console.log(c)
+
+*/
+
+/*
+let nota = parseFloat(prompt("Qual a sua nota? \n"))
+if(nota >= 7){
+    console.log("Você está aprovado")
+}else if(nota >= 5){
+    console.log("Você está em recuperação")
+}
+else{
+    console.log("REPROVADO")
+}
+*/
+    
+
+
+
+
+
+
+
+
+
+
